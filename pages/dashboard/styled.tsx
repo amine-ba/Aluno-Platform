bab66999b6101c5b4724ca33711d88be2707426e
Amine <ev18663@bristol.ac.uk>
2021-04-23 14:41:19 +0100

    Fix Error

--- pages/dashboard/styled.tsx
+++ pages/dashboard/styled.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import { Layout, Menu } from "antd";
 const { Header, Sider, Content } = Layout;
 
-export const StyledLayout = styled.Layout`
+export const StyledLayout = styled(Layout)`
   background: #fff;
 `;
 
-export const StyledContent = styled.Content`
+export const StyledContent = styled(Content)`
   margin: "24px 16px";
   padding: 24;
   min-height: 280;
+  background-color: #fff;
 `;
